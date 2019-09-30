ac881108fbe4641639b1b1824f9d70bc285756a9
MHuanterAxe <maks.shkm@gmail.com>
2019-09-30 14:34:35 +0300

    0.0.3

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        pages: [
+            {page : "Home", link:"#"},
+            {page : "Pages", link:"#"},
+            {page : "Features", link:"#"},
+            {page : "Extentions", link:"#"},
+            {page : "Tutorials", link:"#"},
+            {page : "Contact us", link:"#"}            
+        ]
+    },
+    methods:{
+
+    }
+})
